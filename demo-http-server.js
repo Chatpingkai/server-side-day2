54fde5bf2baaa4121ef76269a8c647785c651a19
Chatpingkai <i0650566653@gmail.com>
2025-07-17 15:01:43 +0700

    Initial project version

---
+++ demo-http-server.js
@@ -0,0 +1,19 @@
+const http = require('http');
+const hostname = 'localhost';
+const port = 3000;
+const server = http.createServer((req, res) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type' ,'text/plain');
+
+        if(req.url === '/about') {
+                res.end('/about');
+        } else if (req.url === '/contact'){
+                res.end('/Contact Page\n');
+        } else{
+                res.end('Hello, World!\n');   
+        }
+});
+
+server.listen(port, hostname, () => {
+            console.log(`Server running at http://${hostname}:${port}/`)
+});
